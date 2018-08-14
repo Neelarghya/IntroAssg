cf35acca0e8829da17517047f7937bab26cc22a6
Neelarghya <neelarghyamandal@gmail.com>
2018-08-14 22:28:41 +0530

    Added Prime Factors Exercise

--- src/com/company/PrimeFactors.java
+++ src/com/company/PrimeFactors.java
@@ -1,5 +1,22 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class PrimeFactors {
+    public PrimeFactors() {
+
+    }
+
+    private ArrayList<Integer> generate(int n){
+        ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+        for (int i = 2 ; i <= Math.ceil(Math.sqrt(n)) ; i++){
+            if (generate(i).size() == 1 && n % i == 0){
+                primeFactors.add(i);
+            }
+        }
+
+        return primeFactors;
+    }
+
 
 }
