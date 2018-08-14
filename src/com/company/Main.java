e1511a9e6b92fef3c409638dec95b3d451e04e98
Neelarghya <neelarghyamandal@gmail.com>
2018-08-14 23:54:26 +0530

    Main method configured

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,9 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        PrimeFactors primeFactors = new PrimeFactors();
-        System.out.println("Enter n: ");
-        int n = in .nextInt();
-        System.out.println(primeFactors.generate(n));
+        AccessExercise accessExercise = new AccessExercise();
+        accessExercise.chooseExercise();
     }
 }
