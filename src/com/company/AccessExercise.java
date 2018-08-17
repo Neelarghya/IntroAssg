e5cba6b4de42b823eef7c22e685a0cfbf00f135f
Neelarghya <neelarghyamandal@gmail.com>
2018-08-17 12:49:19 +0530

    Display formatting

--- src/com/company/AccessExercise.java
+++ src/com/company/AccessExercise.java
@@ -27,41 +27,41 @@ public class AccessExercise {
                     System.out.println("Astrix:");
                     triangle.printAstrix();
 
-                    System.out.println("\nHorizontal Line:\nEnter n: ");
+                    System.out.print("\nHorizontal Line:\nEnter n: ");
                     n = in.nextInt();
                     triangle.printHorizontalLine(n);
 
-                    System.out.println("\nVertical Line:\nEnter n: ");
+                    System.out.print("\nVertical Line:\nEnter n: ");
                     n = in.nextInt();
                     triangle.printVerticalLine(n);
 
-                    System.out.println("\nRight Triangle:\nEnter n: ");
+                    System.out.print("\nRight Triangle:\nEnter n: ");
                     n = in.nextInt();
                     triangle.printRightTriangle(n);
                     break;
 
                 case 2:
-                    System.out.println("\nIsosceles Triangle:\nEnter n: ");
+                    System.out.print("\nIsosceles Triangle:\nEnter n: ");
                     n = in.nextInt();
                     diamond.printIsoscelesTriangle(n);
 
-                    System.out.println("\nDiamond:\nEnter n: ");
+                    System.out.print("\nDiamond:\nEnter n: ");
                     n = in.nextInt();
                     diamond.printDiamond(n);
 
-                    System.out.println("\nDiamond with Name:\nEnter n: ");
+                    System.out.print("\nDiamond with Name:\nEnter n: ");
                     n = in.nextInt();
                     diamond.printDiamondWithName(n);
                     break;
 
                 case 3:
-                    System.out.println("\nFizz Buzz Exercise:\nEnter n:");
+                    System.out.print("\nFizz Buzz Exercise:\nEnter n: ");
                     n = in.nextInt();
                     fizzBuzz.runFizzBuzz(n);
                     break;
 
                 case 4:
-                    System.out.println("Enter the number whose Prime factors you want to generate: ");
+                    System.out.print("Enter the number whose Prime factors you want to generate: ");
                     n = in.nextInt();
                     primeFactors.printPrimeFactors(n);
                     break;
